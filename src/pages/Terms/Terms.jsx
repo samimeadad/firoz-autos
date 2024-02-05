7edb4379e9456667957214b986e52373dd23459f
samimeadad <sami.meadad@gmail.com>
2024-02-05 14:55:52 +0600

    Terms & Condition page done

--- src/pages/Terms/Terms.jsx
+++ src/pages/Terms/Terms.jsx
@@ -1,14 +1,60 @@
-import React from 'react';
 import Header from '../../components/Header/Header';
 import FooterComponent from '../../components/Footer/FooterComponent';
 
 const Terms = () => {
     return (
-        <div>
+        <section>
             <Header />
-            <h1>Terms & Conditions</h1>
+            <section className='my-20 container mx-auto text-justify'>
+                <h1 className='text-5xl font-bold text-center my-10'>Terms & Condition</h1>
+                <p className='text-xl text-justify mt-20 mb-5'>
+                    <h2 className='text-center text-4xl font-bold'>About this Site</h2>
+                </p>
+                <p className='text-xl text-center'>
+                    An Overview to the Firoz Autos Site
+                </p>
+                <p className='text-xl text-justify mt-20 mb-5'>
+                    <h2 className='text-center text-4xl font-bold'>About the Firoz Autos Site</h2>
+                </p>
+                <p className='text-xl text-justify my-10'>
+                    The "Firoz Autos" website (https://firoz-autos.com/ the“Website”) managed by Firoz Autos Ltd. keeps you up to date with Firoz Autos local activities, products and services, and more. If you would like to link to our site, please use the text "The Firoz Autos Site" or "Firoz Autos" and set the url to https://firoz-autos.com/
+                </p>
+                <p className='text-xl text-justify my-10'>
+                    <h2 className='text-2xl font-bold'>Legal Notice</h2>
+                </p>
+                <p className='text-xl text-justify my-10'>
+                    This Website is owned, operated and maintained by or for Firoz Autos Ltd.("Firoz Autos"). You can use this web site only if you agree to and accept the Terms and Conditions without limitation or reservation.
+                </p>
+                <p className='text-xl text-justify my-10'>
+                    <h2 className='text-2xl font-bold'>Limitation of Use</h2>
+                </p>
+                <p className='text-xl text-justify my-10'>
+                    All material on this Web site is protected by copyright laws, and other applicable laws of each country throughout the world. Except for personal, non-commercial internal use, you are prohibited to use (including without limitation, copying, modifying, reproducing in whole or in part, uploading, transmitting, distributing, licensing, selling and publishing) any of the materials, without obtaining Firoz Autos's prior express permission.
+                </p>
+                <p className='text-xl text-justify my-10'>
+                    <h2 className='text-2xl font-bold'>Trademarks</h2>
+                </p>
+                <p className='text-xl text-justify my-10'>
+                    The trademark "Firoz Autos" and any other product names, service names or logos of Firoz Autos used, quoted and/or referenced in this Web Site are trademarks or registered trademarks of Firoz Autos or any of its affiliates. Other product names and/or company names used in the Web Site may be protected as their trademarks and/or trade names.
+                </p>
+                <p className='text-xl text-justify my-10'>
+                    <h2 className='text-2xl font-bold'>Disclaimers of Warranties</h2>
+                </p>
+                <p className='text-xl text-justify my-10'>
+                    Except to the extent prohibited by applicable laws, Firoz Autos and/or any of its affiliates make no representation and warranty, either express or implied, including warranties or representations with respect to the accuracy, reliability, completeness, fitness for particular purpose, non - infringement of third parties rights and/or safety of the contents of this Website. Firoz Autos and/or any of its affiliates shall not be liable for any loss or damage arising out of your use or access, or inability to use or access, this Web Site.
+                </p>
+                <p className='text-xl text-justify my-10'>
+                    <h2 className='text-2xl font-bold'>Applicable Laws</h2>
+                </p>
+                <p className='text-xl text-justify my-10'>
+                    This Terms and Conditions shall be governed by and construed in accordance with the laws of Japan unless otherwise expressly stated.
+                </p>
+                <p className='text-xl text-justify my-10'>
+                    We reserve the right to amend this Terms and Conditions from time to time. Any changes will be posted within this page and shall become effective as soon as they are posted.
+                </p>
+            </section>
             <FooterComponent />
-        </div>
+        </section>
     );
 };
 
