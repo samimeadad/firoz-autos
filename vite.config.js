47f4ff192b97d328f4e062415a5245394b6bcecf
Sami Meadad Choudhury <sami.meadad@gmail.com>
2025-07-07 21:49:39 +0600

    vercel.json file added for vercel hosting

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig( {
   plugins: [ react() ],
-  //base: '/firoz-autos/' // Set the base path for GitHub Pages
+  base: '/firoz-autos/' // Set the base path for GitHub Pages
 } )
